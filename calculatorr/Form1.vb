b5c3afbf7e942eb67504cd86e428b129d8d12a04
HIBA TEMIM <hibatemim@isgb.ucar.tn>
2025-10-31 15:06:57 +0100

    travail réalisé par Hiba Tmime

---
+++ calculatorr/Form1.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles result.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        btn1.Text = btn1.Text + "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        TextBox1.Text = TextBox1.Text + "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        TextBox1.Text = TextBox1.Text + "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        TextBox1.Text = TextBox1.Text + "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        TextBox1.Text = TextBox1.Text + "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        TextBox1.Text = TextBox1.Text + "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        TextBox1.Text = TextBox1.Text + "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        TextBox1.Text = TextBox1.Text + "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        TextBox1.Text = TextBox1.Text + "9"
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        TextBox1.Text = TextBox1.Text + "0"
+    End Sub
+
+    Private Sub btnp_Click(sender As Object, e As EventArgs) Handles btnp.Click
+        TextBox1.Text = TextBox1.Text + "."
+    End Sub
+
+    Private Sub btn00_Click(sender As Object, e As EventArgs) Handles btn00.Click
+        TextBox1.Text = TextBox1.Text + "00"
+    End Sub
+End Class
